555cf94d41eb2c1a17f7927e44b161de903912cd
Mahima Devi <72203327+mahimadevi@users.noreply.github.com>
2022-02-10 03:17:20 -0800

    Time: 11 ms (44.03%), Space: 13.9 MB (27.09%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -13,3 +13,10 @@ public:
         return maxProduct;
     }
 };
+// agr hmara abhi tak ka product positive hai, aur abhi jissey multiply kr rhe wo bhi positive hai to hmara product aur increase hoga.
+//agr hmara abhi tak product negative hai, aur abhi jissey multiply kr rhe wo bhi negative hai to hmara product aur increase hoga, mgr agr bhi jissey multiply kr rhe positive hua to aise mein hmara product decrease hoga.
+// agr hmara abhi tak ka product zero hai,aur abhi jissey multiply kr rhw wo positive ya negative kuch bhhi to hmara product 0 hi aayega.
+
+//hmne 3 variables banaye, max_so_far jisme hm abhi tak ki max value store krenge, min_so_far jisme hm abhi tak ki min value store krenge, and maxProduct jisme hm overall max value store krenge.
+
+//agr abhi bhi nhi samjh aa rha to ye video dekh lena: https://www.youtube.com/watch?v=hJ_Uy2DzE08&ab_channel=KnowledgeCenter
