bc43469e5ce39db8b52210e4601057ecf562411b
Mahima Devi <72203327+mahimadevi@users.noreply.github.com>
2022-02-12 06:39:16 -0800

    Time: 95 ms (72.36%), Space: 20 MB (86.87%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -5,21 +5,21 @@ public:
         sort(nums.begin(),nums.end());
         
         vector<vector<int>> res;
+        // skiping all continous same element, doing bcz to avoid duplicate triplets
         for(int i=0; i < n-2 ;i++){
             if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+                // two pointer's approach
+                // a + b + c = 0, so we can make the equation like
+                // b + c = -a;
+                // low + high = -sum;
                 int low=i+1;
                 int high=nums.size()-1;
-                int sum=0-nums[i];
+                int sum=-nums[i];
                 
                 while(low<high){
                     if(nums[low] + nums[high] == sum){
                         res.push_back({nums[i], nums[low], nums[high]});
-                       /*vector<int> temp;
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[low]);
-                        temp.push_back(nums[high]);
-                        res.push_back(temp);*/
-                        
+                  
                         while(low<high && nums[low]==nums[low+1]) low++;
                         while(low<high && nums[high]==nums[high-1]) high--;
                         
