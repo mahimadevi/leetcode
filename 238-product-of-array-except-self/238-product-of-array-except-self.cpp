ff809a1dc054d5dca0ef379e0e447e4a02c76b35
Mahima Devi <72203327+mahimadevi@users.noreply.github.com>
2022-02-13 00:44:21 -0800

    Time: 16 ms (98.34%), Space: 24.9 MB (36.26%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size(),initialProduct=1,res,prod=1,count=0;
-        vector<int>ans;
+        int n = nums.size();
+        vector<int>output;
+        if(n<1)
+            return output;
+        
+        int product = 1;
         for(int i=0;i<n;i++)
         {
-            if(nums[i]==0)
-                count++;
-            else
-                prod=prod*nums[i];
+            product=product*nums[i];
+            output.push_back(product);
         }
-        for(int i=0;i<n;i++)
+        product=1;
+        for(int i=n-1;i>0;--i)
         {
-            if(count>1) //ek se zyada 0 hue to sb zero honge
-                res=0;
-            
-            else if(count==1 && nums[i]!=0)//agr sirf ek hi 0 hua to baaki sbhi jagah 0 product
-                res=0;
-            
-            else if(count==1 && nums[i]==0)//agr sirf ek hi 0 hua to ussi index pr sirf value hogi baaki jagah 0 hoga
-                res=prod;
-            
-            else    //baaki sbhi cases mein product/nums[i] hoga
-                res=prod/nums[i];
-            
-            ans.push_back(res);
+            output[i] = output[i-1] * product;
+            product = product*nums[i];
         }
-        return ans;
+        output[0] = product;
+        
+        return output;
     }
 };
