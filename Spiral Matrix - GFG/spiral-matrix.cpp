8996aaa1d9aae740c0ea535e1da03f3458674e0a
Mahima Devi <72203327+mahimadevi@users.noreply.github.com>
2023-06-08 14:15:54 +0530

    Added solution - LeetHub

---
+++ Spiral Matrix - GFG/spiral-matrix.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+	public:
+	int findK(vector<vector<int>> &mat, int n, int m, int k)
+    {
+        // Your code goes here
+        vector<int> ans;
+ 
+  
+  
+  // Initialize the pointers reqd for traversal.
+  int top = 0, left = 0, bottom = n - 1, right = m - 1;
+
+  // Loop until all elements are not traversed.
+  while (top <= bottom && left <= right) {
+      
+    // For moving left to right
+    for (int i = left; i <= right; i++)
+      ans.push_back(mat[top][i]);
+
+    top++;
+
+    // For moving top to bottom.
+    for (int i = top; i <= bottom; i++)
+      ans.push_back(mat[i][right]);
+
+    right--;
+    
+    // For moving right to left.
+    if (top <= bottom) {
+      for (int i = right; i >= left; i--)
+       ans.push_back(mat[bottom][i]);
+
+      bottom--;
+    }
+
+    // For moving bottom to top.
+    if (left <= right) {
+      for (int i = bottom; i >= top; i--)
+        ans.push_back(mat[i][left]);
+
+      left++;
+    }
+  }
+ return ans[k-1];
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+    cin>>T;
+  
+    while(T--)
+    {
+        int n,m;
+        int k=0;
+        //cin>>k;
+        cin>>n>>m>>k;
+        vector<vector<int>> a(n, vector<int>(m, 0));
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cin>>a[i][j];
+            }
+        }
+
+        Solution obj;
+
+        cout<< obj.findK(a, n, m, k) << "\n";
+        
+       
+    }
+}
+// } Driver Code Ends
