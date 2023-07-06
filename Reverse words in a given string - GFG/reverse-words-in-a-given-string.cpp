0fc90555553cd707a54202526ec1978c76b13c49
Mahima Devi <72203327+mahimadevi@users.noreply.github.com>
2023-07-06 15:16:03 +0530

    Added solution - LeetHub

---
+++ Reverse words in a given string - GFG/reverse-words-in-a-given-string.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to reverse words in a given string.
+    string reverseWords(string s) 
+    { 
+        // code here 
+        if(s.size() == 0) return s;
+        stack<string> stack;
+        string result;
+        for(int i=0; i<s.size(); i++) {
+            string word;
+            if(s[i]=='.') continue; //skip spaces
+            while(i<s.size() && s[i]!='.' ) { //store continuous letters into word
+                word += s[i]; i++;
+            }
+            stack.push(word); //push word to the stack
+        }
+        while(!stack.empty()) {
+            result += stack.top(); stack.pop();
+            if(!stack.empty()) result += ".";
+        }
+        return result;
+    } 
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout<<obj.reverseWords(s)<<endl;
+    }
+}
+// } Driver Code Ends
