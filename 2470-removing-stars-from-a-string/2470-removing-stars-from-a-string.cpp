b5db71105d90a24d94f5dab54620e7b9eb160619
Mahima Devi <72203327+mahimadevi@users.noreply.github.com>
2025-02-03 15:14:52 +0530

    Time: 5 ms (96.84%), Space: 26.9 MB (89.97%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
@@ -1,23 +1,42 @@
+// class Solution {
+// public:
+//     string removeStars(string s) {
+//         // Use a stack (or a string acting as a stack) to process characters
+//         string result;
+
+//         // Iterate through each character in the string
+//         for (char c : s) {
+//             if (c == '*') {
+//                 // If the character is '*', remove the last added character (if any)
+//                 if (!result.empty()) {
+//                     result.pop_back();
+//                 }
+//             } else {
+//                 // Otherwise, add the character to the result
+//                 result.push_back(c);
+//             }
+//         }
+
+//         // Return the final processed string
+//         return result;
+//     }
+// };
 class Solution {
 public:
     string removeStars(string s) {
-        // Use a stack (or a string acting as a stack) to process characters
-        string result;
-
-        // Iterate through each character in the string
-        for (char c : s) {
-            if (c == '*') {
-                // If the character is '*', remove the last added character (if any)
-                if (!result.empty()) {
-                    result.pop_back();
-                }
+        int i = 0, j = 0; // `i` iterates through the string, `j` keeps track of valid characters
+        
+        for (i = 0; i < s.size(); i++) {
+            if (s[i] == '*') {
+                // If we find a '*', remove the last added character by moving `j` back
+                j--;
             } else {
-                // Otherwise, add the character to the result
-                result.push_back(c);
+                // Otherwise, store the current character at position `j` and move `j` forward
+                s[j++] = s[i];
             }
         }
-
-        // Return the final processed string
-        return result;
+        
+        // Return only the valid part of the string (up to `j`)
+        return s.substr(0, j);
     }
 };
