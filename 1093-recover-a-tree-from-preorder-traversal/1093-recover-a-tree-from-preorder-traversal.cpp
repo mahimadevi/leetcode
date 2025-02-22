4f57dd0fb2be7fa86fa1abf405862f25c89dc82a
Mahima Devi <72203327+mahimadevi@users.noreply.github.com>
2025-02-22 11:18:05 +0530

    Time: 3 ms (62.8%), Space: 20.8 MB (77.02%) - LeetHub

--- 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.cpp
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.cpp
@@ -9,48 +9,52 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
- class Solution {
+class Solution {
 public:
-    TreeNode* recoverFromPreorder(string_view s) {
-        vector<TreeNode*> path;
-        size_t pos = 0;
+    TreeNode* recoverFromPreorder(string s) {
+        // Stack to keep track of our path in the tree
+        vector<TreeNode*> stack;
         
-        while (pos < s.length()) {
-            // Get level and value together
-            size_t level = 0;
-            int val = 0;
-            
-            // Count level (dashes)
-            while (pos < s.length() && s[pos] == '-') {
+        for(int i = 0; i < s.length();) {
+            // Count the dashes to get depth
+            int level = 0;
+            while(i < s.length() && s[i] == '-') {
                 level++;
-                pos++;
+                i++;
             }
             
-            // Get value
-            while (pos < s.length() && s[pos] != '-') {
-                val = val * 10 + (s[pos] - '0');
-                pos++;
+            // Parse the number
+            int value = 0;
+            while(i < s.length() && s[i] != '-') {
+                value = value * 10 + (s[i] - '0');
+                i++;
             }
             
-            // Resize path to correct level
-            while (path.size() > level) {
-                path.pop_back();
+            // Create new node
+            TreeNode* node = new TreeNode(value);
+            
+            // If stack is empty, this is the root
+            if(stack.empty()) {
+                stack.push_back(node);
+                continue;
             }
             
-            // Create and link new node
-            TreeNode* node = new TreeNode(val);
-            if (!path.empty()) {
-                if (!path.back()->left) {
-                    path.back()->left = node;
-                } else {
-                    path.back()->right = node;
-                }
+            // Pop from stack until we reach the parent
+            while(stack.size() > level) {
+                stack.pop_back();
             }
             
-            path.push_back(node);
+            // Attach node to parent
+            if(!stack.back()->left) {
+                stack.back()->left = node;
+            } else {
+                stack.back()->right = node;
+            }
+            
+            // Add current node to stack
+            stack.push_back(node);
         }
         
-        return path[0];
+        return stack[0];  // Return root
     }
 };
